78aea9670df6eab4f4f4992758c5acaa02ecfbae
Rebel Cat <rebelcat12@protonmail.com>
2024-05-06 21:13:39 -0400

    First commit, add main file and todo

---
+++ OOPpractice.lua
@@ -0,0 +1,10 @@
+local function Pet(name)
+    return {
+        name = "Charlie"
+    }
+end
+
+local cat = Pet()
+local dot = Pet()
+
+print(cat.name)
---
+++ main.lua
@@ -0,0 +1,9 @@
+--Player varibales--
+local playerHealth = 10
+local playerBaseDamage = 3
+local playerInventory = {}
+--Enemy variables--
+local enemyHealth = 11
+local enemyBaseDamage = 2
+local enemyInventory  = {}
+
