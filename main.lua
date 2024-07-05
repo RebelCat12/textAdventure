d1fb2460f667f56220fd6f0dd5033531cab6d695
Rebel Cat <rebelcat12@protonmail.com>
2024-07-04 22:41:39 -0400

    Refactored npc dialogue function

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-local healthPotion = require("healthPotion")
+--Text based RPG game
 
 local function Player(name)
     return {
         name = name or "Daniel",
-        health = 100,
-        damage = 10,
+        health = 25,
+        damage = 4,
         gold = 0,
         level = 0,
         inventory = {}
@@ -14,28 +14,34 @@ end
 local function Enemy(type)
     return {
         type = type or "goblin",
-        health = 70,
-        damage = 12,
+        health = 15,
+        damage = 6,
         --Find a way to make the enemy level reference the instance of a player, not the base object.
 
-        --[[The specific enemy instance will inherit this function and the amount of gold will depend on the
-            type and level of the enemy.]]
+        --[[
+            The specific enemy instance will inherit this function and the amount of gold will depend on the
+            type and level of the enemy.
+            ]]
         inventory = {}
     }
 end
 
 local function NPC()
     return {
-        dialogue = {"Hello", "Hi", "How are you"},
-
-        --This is a bit messy, but it works!
-        --[[Namely, the messy part is that we are creating an instance of the parent function inside
+        --[[
+            !!LEGACY COMMENT, IT'S ONLY HERE BECAUSE IT'S FUNNIE!!
+            Namely, the messy part is that we are creating an instance of the parent function inside
             of this function. I'm sure there must be a way to refactor this so that it's cleaner, but
-            I haven't found it yet.]]
+            I haven't found it yet.
+
+            FIXED IT. I'm going to leave this comment here because it's funny, but I've moved stuff around
+            so that the function isn't being called inside of itself anymore, which definitely seems like an inprovement.
+            ]]
         sayHello = function (self)
-            local sentance = NPC()
-            local random = math.random(1, #sentance.dialogue)
-            print(sentance.dialogue[random])
+            math.randomseed(os.time())
+            local dialogue = {"Hello", "Hi", "How are you"}
+            local random = math.random(1, #dialogue)
+            print(dialogue[random])
         end
     }
 end
